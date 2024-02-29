5fd16d36e760473f838661e440abd64383f54924
Tanmay Arora <97460992+tanmay-0017@users.noreply.github.com>
2024-02-29 17:00:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-const port = 3003;
+const port = process.env.PORT || 3003;
 const dbConnect = require("./dbConnect.js");
 const cors = require('cors');
 app.use(cors());
